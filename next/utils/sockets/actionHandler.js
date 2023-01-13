a5b47b793a2b25589cd4a230ff4d078a4268895d
klxu03 <beaver2025@gmail.com>
2023-01-13 22:10:51 +1300

    kevin/kev-64-handle-actions

--- next/utils/sockets/actionHandler.js
+++ next/utils/sockets/actionHandler.js
@@ -1,18 +1,17 @@
 // Handles an action
 
 import { setCards } from "../card";
-import { startTurn } from "../game";
+import { startRound } from "../game";
 
 /* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
 export default (io, socket, db) => {
   const start = (gameId) => {
     socket.emit("start"); // not a broadcast since even sender needs to know
 
-    db.data.games[gameId].turn = -1;
     db.data.games[gameId].blind = -1;
 
     setCards({ gameId, io, db });
-    startTurn({ gameId, io, socket, db });
+    startRound({ gameId, io, socket, db });
 
     db.write();
   };
